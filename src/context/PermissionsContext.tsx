e6e94d54d654037524508a1f528eb91eb8241933
Douglas Juarez <dfjuarezreyes21@gmail.com>
2022-02-15 10:56:43 -0600

    LoadingScreen added, PermissionContext with ask and check permissions, validation block permission

--- src/context/PermissionsContext.tsx
+++ src/context/PermissionsContext.tsx
@@ -1,5 +1,7 @@
-import { Children, createContext, useState } from "react";
-import { PermissionStatus } from "react-native-permissions";
+import React, { useEffect } from 'react';
+import { createContext, useState } from "react";
+import { AppState, ImageStore, Platform } from 'react-native';
+import { PERMISSIONS, PermissionStatus, request, check, openSettings } from "react-native-permissions";
 
 export interface PermissionsState {
     locationStatus: PermissionStatus;
@@ -19,15 +21,66 @@ export const PermissionContext = createContext({} as PermissionsContextProps);
 
 export const PermissionsProvider = ({ children }: any) => {
 
-    const askLocationPermission = () => {
-
-    }
-
-    const checkLocationPermission = () => {
-
-    }
-
     const [permissions, setPermissions] = useState(permissionInitState);
+
+    useEffect(() => {
+        
+        AppState.addEventListener('change', ( state ) => {
+            if(state !== 'active') return;
+
+            checkLocationPermission();
+        });
+      
+    }, [])
+    
+
+    const askLocationPermission = async () => {
+        
+        let permissionsStatus: PermissionStatus;
+                
+        if(Platform.OS == 'ios'){
+
+            // permissionsStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+            permissionsStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+            
+        }else{
+            
+            // permissionsStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+            permissionsStatus = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+        }
+
+        if( permissionsStatus === 'blocked'){
+            openSettings();
+        }
+
+        setPermissions({
+            ...permissions,
+            locationStatus: permissionsStatus
+        });
+        
+    }
+
+    const checkLocationPermission = async () => {
+
+        let permissionsStatus: PermissionStatus;
+                
+        if(Platform.OS == 'ios'){
+
+            // permissionsStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+            permissionsStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+            
+        }else{
+            
+            // permissionsStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+            permissionsStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+        }
+
+        setPermissions({
+            ...permissions,
+            locationStatus: permissionsStatus
+        });
+
+    }
     
     return (
         <PermissionContext.Provider
--- src/screens/PermissionsScreen.tsx
+++ src/screens/PermissionsScreen.tsx
@@ -1,34 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Platform, StyleSheet, Text, View } from 'react-native';
 import { check, PERMISSIONS, PermissionStatus, request } from 'react-native-permissions';
+import { PermissionContext } from '../context/PermissionsContext';
 
 export const PermissionsScreen = () => {
 
-    let permissionsStatus: PermissionStatus;
-    
-    const checkLocationPermission = async () => {
-        
-        if(Platform.OS == 'ios'){
+    const { permissions, checkLocationPermission, askLocationPermission } = useContext(PermissionContext);
 
-            // permissionsStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-            permissionsStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-            
-        }else{
-            
-            // permissionsStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-            permissionsStatus = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-        }
-
-        console.log({permissionsStatus});
-    }
 
     return (
         <View style={ style.container }>
             <Text>PermisssionsScreen</Text>
             <Button 
                 title='Permiso'
-                onPress={ checkLocationPermission }
+                onPress={ askLocationPermission }
             />
+            <Text>{ JSON.stringify({ permissions }, null, 5) }</Text>
         </View>
     )
 }
